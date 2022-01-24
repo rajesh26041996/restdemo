907308019388b433c73ea4346f09bd2957397910
Rajesh kumar <rajesh@infrrd.ai>
2022-01-24 22:30:04 +0530

    uncommented

--- src/main/java/ai/qa/demo/Upload.java
+++ src/main/java/ai/qa/demo/Upload.java
@@ -32,56 +32,56 @@ public class Upload {
 //            e.printStackTrace();
 //        }
 
-//        //read_data
-//        String read=given().header("Content-Type","application/json").queryParam("id","1").when().get("read").then().extract().response().asString();
-//        System.out.println(read);
-//
-//        //readall
-//        String  readall=given().header("Content-Type","application/json").when().get("readall").then().extract().response().asString();
-//        System.out.println(readall);
-//
-//        //write_data
-//        String write=given().header("Content-Type","application/json").body("{\n" +
-//                "    \"id\":\"5\",\n" +
-//                "    \"name\":\"rajeshKumar\"\n" +
-//                "}").when().post("write").then().extract().response().asString();
-//        System.out.println(write);
-//
-//
-//        //update
-//        String update=given().header("Content-Type","application/json").body("{\n" +
-//                "    \"id\":\"5\",\n" +
-//                "    \"name\":\"raj\"\n" +
-//                "}").when().put("update").then().extract().response().asString();
-//        System.out.println(update);
-//
-//
-//
-//        //delete
-//        ValidatableResponse delete=given().header("Content-Type","application/json").body("{\n" +
-//                "    \"id\":\"7\"\n" +
-//                "   \n" +
-//                "}").when().delete("delete").then().assertThat().statusCode(200).header("server","Werkzeug/2.0.1 Python/3.8.3");
-//        System.out.println(delete);
+        //read_data
+        String read=given().header("Content-Type","application/json").queryParam("id","1").when().get("read").then().extract().response().asString();
+        System.out.println(read);
+
+        //readall
+        String  readall=given().header("Content-Type","application/json").when().get("readall").then().extract().response().asString();
+        System.out.println(readall);
+
+        //write_data
+        String write=given().header("Content-Type","application/json").body("{\n" +
+                "    \"id\":\"5\",\n" +
+                "    \"name\":\"rajeshKumar\"\n" +
+                "}").when().post("write").then().extract().response().asString();
+        System.out.println(write);
+
+
+        //update
+        String update=given().header("Content-Type","application/json").body("{\n" +
+                "    \"id\":\"5\",\n" +
+                "    \"name\":\"raj\"\n" +
+                "}").when().put("update").then().extract().response().asString();
+        System.out.println(update);
+
+
+
+        //delete
+        ValidatableResponse delete=given().header("Content-Type","application/json").body("{\n" +
+                "    \"id\":\"7\"\n" +
+                "   \n" +
+                "}").when().delete("delete").then().assertThat().statusCode(200).header("server","Werkzeug/2.0.1 Python/3.8.3");
+        System.out.println(delete);
 
 
 
 
 
         //cookie
-        SessionFilter sessionFilter=new SessionFilter();
-        RestAssured.baseURI="https://gateway-stage-idc.infrrdapis.com/";
-        String loginResource="authentication/v1/login";
-        String uploadResource="upload/v2/user-model/558e6bc0-b1ae-4a96-8f02-b0d24f387856";
-        String filetobeUploaded="D:\\166c8a8e-e27a-4a46-92c1-7ffd547c6a6c_document-1_split-2.pdf";
-        String login=given().filter(sessionFilter).header("Content-Type","application/json").body("{\n" +
-                "    \"password\": \"Raj@2604\",\n" +
-                "        \"userName\": \"rajesh+admin@infrrd.ai\"\n" +
-                "}").when().post(loginResource).then().extract().response().asString();
-        System.out.println(login);
-
-        String upload=given().filter(sessionFilter).header("Content-Type","multipart/form-data").multiPart(new File(filetobeUploaded)).when().post(uploadResource).then().extract().response().asString();
-        System.out.println(upload);
+//        SessionFilter sessionFilter=new SessionFilter();
+//        RestAssured.baseURI="https://gateway-stage-idc.infrrdapis.com/";
+//        String loginResource="authentication/v1/login";
+//        String uploadResource="upload/v2/user-model/558e6bc0-b1ae-4a96-8f02-b0d24f387856";
+//        String filetobeUploaded="D:\\166c8a8e-e27a-4a46-92c1-7ffd547c6a6c_document-1_split-2.pdf";
+//        String login=given().filter(sessionFilter).header("Content-Type","application/json").body("{\n" +
+//                "    \"password\": \"Raj@2604\",\n" +
+//                "        \"userName\": \"rajesh+admin@infrrd.ai\"\n" +
+//                "}").when().post(loginResource).then().extract().response().asString();
+//        System.out.println(login);
+//
+//        String upload=given().filter(sessionFilter).header("Content-Type","multipart/form-data").multiPart(new File(filetobeUploaded)).when().post(uploadResource).then().extract().response().asString();
+//        System.out.println(upload);
 
     }
 
