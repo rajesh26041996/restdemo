fa3d08e75b0d4e4589d9c4d47b4b1fa3feb1afc0
Rajesh kumar <rajesh@infrrd.ai>
2022-01-25 12:10:37 +0530

    test4

--- src/main/java/ai/qa/demo/Upload.java
+++ src/main/java/ai/qa/demo/Upload.java
@@ -11,7 +11,7 @@ public class Upload {
     public static void main(String[] args)  {
 
 
-        System.out.println("Test");
+
         RestAssured.baseURI="http://0.0.0.0:1144";
         //upload file
 //        String filepath="D:/166c8a8e-e27a-4a46-92c1-7ffd547c6a6c_document-1_split-2.pdf";
@@ -35,16 +35,17 @@ public class Upload {
 //            e.printStackTrace();
 //        }
 
-        String readId=System.getProperty("readId");
-        System.out.println(readId);
+
+
 
         //read_data
+        String readId=System.getProperty("readId");
         String read=given().header("Content-Type","application/json").queryParam("id",readId).when().get("read").then().extract().response().asString();
-        System.out.println(read);
+        System.out.println("read: "+read);
 
         //readall
         String  readall=given().header("Content-Type","application/json").when().get("readall").then().extract().response().asString();
-        System.out.println(readall);
+        System.out.println("readall: "+readall);
 
         //write_data
         String writeId=System.getProperty("writeId");
@@ -55,24 +56,27 @@ public class Upload {
                 "    \"id\":\""+writeId+"\",\n" +
                 "    \"name\":\""+writeName+"\"\n" +
                 "}").when().post("write").then().extract().response().asString();
-        System.out.println(write);
+        System.out.println("write: "+write);
 
 
         //update
+        String updateId=System.getProperty("updateId");
+        String updateName=System.getProperty("updateName");
         String update=given().header("Content-Type","application/json").body("{\n" +
-                "    \"id\":\"5\",\n" +
-                "    \"name\":\"raj\"\n" +
+                "    \"id\":\""+updateId+"\",\n" +
+                "    \"name\":\""+updateName+"\"\n" +
                 "}").when().put("update").then().extract().response().asString();
-        System.out.println(update);
+        System.out.println("update: "+update);
 
 
 
         //delete
-        ValidatableResponse delete=given().header("Content-Type","application/json").body("{\n" +
+        String deleteId=System.getProperty("deleteId");
+        String delete=given().header("Content-Type","application/json").body("{\n" +
                 "    \"id\":\"7\"\n" +
                 "   \n" +
-                "}").when().delete("delete").then().assertThat().statusCode(200);
-        System.out.println(delete);
+                "}").when().delete("delete").then().extract().response().asString();
+        System.out.println("delete: "+delete);
 
 
 
