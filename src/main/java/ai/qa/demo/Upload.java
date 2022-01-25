09cbc44ec019dc517d304d159202a43a7797308e
Rajesh kumar <rajesh@infrrd.ai>
2022-01-25 11:30:27 +0530

    test3

--- src/main/java/ai/qa/demo/Upload.java
+++ src/main/java/ai/qa/demo/Upload.java
@@ -11,6 +11,7 @@ public class Upload {
     public static void main(String[] args)  {
 
         System.out.println(System.getProperty("Env"));
+        System.out.println(System.getProperty("readId"));
         System.out.println("Test");
         RestAssured.baseURI="http://0.0.0.0:1144";
         //upload file
